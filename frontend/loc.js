1fbf9ec6cf8d38a796e45aefce379ba43596e877
Byllie <90178612+Byllie@users.noreply.github.com>
2024-06-05 09:57:36 +0200

    Bob
    Bob

--- frontend/loc.js
+++ frontend/loc.js
@@ -1,53 +1,4 @@
 var DIC_LOC = {
-   "Usine": {
-      "Vestiaires": [
-         {
-            "id": "eui-a860000000000000",
-            "x": 2,
-            "y": 6
-         },
-         {
-            "id": "eui-a8610a3222222222",
-            "x": 2,
-            "y": 6
-         }
-      ],
-      "Salle_informatique": [
-         {
-            "id": "eui-a8610a3035555555",
-            "x": 5,
-            "y": 2
-         }
-      ],
-      "Piece_principale": [
-         {
-            "id": "eui-a8610a3231278105",
-            "x": 3,
-            "y": 3
-         }
-      ],
-      "Bureau": [
-         {
-            "id": "eui-a8610a3239999999",
-            "x": 7,
-            "y": 8
-         }
-      ],
-      "Piece_secondaire": [
-         {
-            "id": "eui-a8610a3333333333",
-            "x": 4,
-            "y": 6
-         }
-      ],
-      "Couloir": [
-         {
-            "id": "eui-a8610a333937930f",
-            "x": 4,
-            "y": 3
-         }
-      ]
-   },
    "Exterieur": {
       "null": [
          {
@@ -61,6 +12,60 @@ var DIC_LOC = {
             "id": "eui-a861111111111111",
             "x": 2,
             "y": 0
+         }
+      ]
+   },
+   "Usine": {
+      "Piece_principale": [
+         {
+            "id": "eui-a8610a3231278105",
+            "x": 3,
+            "y": 3
+         }
+      ],
+      "Couloir": [
+         {
+            "id": "eui-a8610a333937930f",
+            "x": 4,
+            "y": 3
+         }
+      ],
+      "Salle_informatique": [
+         {
+            "id": "eui-a8610a3035555555",
+            "x": 5,
+            "y": 2
+         },
+         {
+            "id": "eui-a8610a3036666666",
+            "x": 5,
+            "y": 2
+         }
+      ],
+      "Vestiaires": [
+         {
+            "id": "eui-a860000000000000",
+            "x": 2,
+            "y": 6
+         },
+         {
+            "id": "eui-a8610a3222222222",
+            "x": 2,
+            "y": 6
+         }
+      ],
+      "Piece_secondaire": [
+         {
+            "id": "eui-a8610a3333333333",
+            "x": 4,
+            "y": 6
+         }
+      ],
+      "Bureau": [
+         {
+            "id": "eui-a8610a3239999999",
+            "x": 7,
+            "y": 8
          }
       ]
    }
